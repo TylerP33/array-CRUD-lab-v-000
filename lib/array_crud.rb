ee0bfd7842007cc33be8c1b6cb89d32fdfcb08e9
Tyler Pulse <tyler3321@gmail.com>
2017-11-10 18:51:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  cats = ["fred", "george", "leslie", "am", "arrays!"]
+  cats[3]
 end
 
 def retrieve_first_element_from_array(array)
